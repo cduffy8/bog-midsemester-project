b4f69fcb5187de72157cefedb6588187306dad2d
Connor Duffy <connorpduffy160182@gmail.com>
2022-10-10 21:58:20 -0400

    retrieve type and alternate on info/move

--- index.js
+++ index.js
@@ -4,19 +4,30 @@ let index = 2;
 
 const left = document.getElementById("left");
 const right = document.getElementById("right");
-const moves = document.getElementById("moves");
 const pname = document.getElementById("name");
-const info = document.getElementById("info");
+const disp_box = document.getElementById("disp_box");
+const type = document.getElementById("type");
+const info_b = document.getElementById("info_b");
+const moves_b = document.getElementById("moves_b");
+
+info_b.style.backgroundColor = "green";
+moves_b.style.backgroundColor = "gray";
+
 let image = "need to find the right element for image";
+let infoNmoves = {
+    'info':'',
+    'moves':'' 
+};
 
 get_prev_pokemon();
 
 left.addEventListener("click", () => get_prev_pokemon());
 right.addEventListener("click", () => get_next_pokemon());
+info_b.addEventListener("click", () => get_info());
+moves_b.addEventListener("click", () => get_moves());
 
 function get_next_pokemon() {
     index++;
-    console.log(index);
     fetch(api_url + index)
         .then((response) => response.json())
         .then((data) => parse_data(data));
@@ -25,20 +36,39 @@ function get_next_pokemon() {
 function get_prev_pokemon() {
     if (index > 1) {
         index--;
-        console.log(index);
         fetch(api_url + index)
             .then((response) => response.json())
             .then((data) => parse_data(data));
     }
 }
 
+function get_info() {
+    info_b.style.backgroundColor = "green";
+    disp_box.textContent = infoNmoves['info'];
+    moves_b.style.backgroundColor = "gray";
+}
+
+function get_moves() {
+    info_b.style.backgroundColor = "gray";
+    disp_box.textContent = infoNmoves['moves'];
+    moves_b.style.backgroundColor = "green";
+}
+
+function set_disp_box() {
+    if (moves_b.style.backgroundColor === "green") {
+        disp_box.textContent = infoNmoves['moves'];
+    } else {
+        disp_box.textContent = infoNmoves['info']
+    }
+}
 
 function parse_data(data) {
-    console.log(data);
     image = data["sprites"]['back_default'];
     pname.textContent = data.name;
-    info.textContent = create_info(data.height, data.weight, data.stats);
-    moves.textContent = create_moves(data.moves);
+    infoNmoves['info'] = create_info(data.height, data.weight, data.stats);
+    infoNmoves['moves'] = create_moves(data.moves);
+    set_disp_box();
+    type.textContent = create_types(data['types']);
 }
 
 function create_info(height, weight, stats) {
@@ -60,3 +90,10 @@ function create_moves(moves) {
     return moveList;
 }
 
+function create_types(types) {
+    let typeStr = types[0]['type']['name'];
+    if (types.length > 1) {
+        typeStr += " " + types[1]['type']['name'];
+    }
+    return typeStr;
+}
